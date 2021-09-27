c1f456ea0d399121deffbdde8380445f39ccd946
Nico Keskin <nico_keskin_91@hotmail.de>
2021-09-27 15:00:18 +0200

    add demo metrics and demo metrics scheduler

---
+++ src/main/java/de/nikeskin/prometheusgrafanademo/DemoMetricsScheduler.java
@@ -0,0 +1,19 @@
+package de.nikeskin.prometheusgrafanademo;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DemoMetricsScheduler {
+
+    private final DemoMetrics demoMetrics;
+
+    public DemoMetricsScheduler(DemoMetrics demoMetrics) {
+        this.demoMetrics = demoMetrics;
+    }
+
+    @Scheduled(fixedRate = 1000)
+    public void triggerCustomMetrics() {
+        demoMetrics.getRandomMetricsData();
+    }
+}
