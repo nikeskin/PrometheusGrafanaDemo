e7fe39577fc4a016eb523bd8284760a52b068634
Nico Keskin <nico_keskin_91@hotmail.de>
2021-09-27 14:43:32 +0200

    first commit

---
+++ src/main/java/de/nikeskin/prometheusgrafanademo/PrometheusGrafanaDemoApplication.java
@@ -0,0 +1,13 @@
+package de.nikeskin.prometheusgrafanademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrometheusGrafanaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrometheusGrafanaDemoApplication.class, args);
+    }
+
+}
